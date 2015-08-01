33be37e7cd1c24715002d7e74a47f46e19fddbda
kitworker <kasyanov@gmail.com>
2015-08-01 19:46:59 +0400

    Changes to be committed: 	new file:   gitPush.sh  Changes not staged for commit: 	modified:   test

---
+++ gitPush.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git config --global user.name "kitworker"
+git config --global user.email kasyanov@gmail.com
+git config --global user.password uql282uql
+echo "authentication success!"
+# менаундхлн оепбши пюг # git remote add origin https://github.com/kitworker/CeateRep.git
+git push -u origin master
+echo "push to CeateRep.git"
